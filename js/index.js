9b9a5f595482b60b712242e80090f76b923f4620
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-01-13 22:31:07 +0300

    fix navbar  adaptation (task1) & fix task 2

--- js/index.js
+++ js/index.js
@@ -1,166 +1,166 @@
-var div = document.createElement('div');
-div.className = "flex-container";
-div.id = "fc";
-document.body.appendChild(div);
+// var div = document.createElement('div');
+// div.className = "flex-container";
+// div.id = "fc";
+// document.body.appendChild(div);
 
-var flexRow = document.createElement('div');
-flexRow.className = "row";
-flexRow.id = "row";
-document.getElementById("fc").appendChild(flexRow);
+// var flexRow = document.createElement('div');
+// flexRow.className = "row";
+// flexRow.id = "row";
+// document.getElementById("fc").appendChild(flexRow);
 
-var flexEl1 = document.createElement('div');
-flexEl1.className = "flex-item";
-flexEl1.id = "fe1";
-document.getElementById("row").appendChild(flexEl1);
+// var flexEl1 = document.createElement('div');
+// flexEl1.className = "flex-item";
+// flexEl1.id = "fe1";
+// document.getElementById("row").appendChild(flexEl1);
 
-var img1 = document.createElement('img');
-img1.src = "images/img.jpg";
-img1.className = "card"
-document.getElementById("fe1").appendChild(img1);
+// var img1 = document.createElement('img');
+// img1.src = "images/img.jpg";
+// img1.className = "card"
+// document.getElementById("fe1").appendChild(img1);
 
-var textOnImage = document.createElement('p');
-textOnImage.className = "imageOnText";
-textOnImage.innerHTML = "LeverX International";
-document.getElementById("fe1").appendChild(textOnImage);
+// var textOnImage = document.createElement('p');
+// textOnImage.className = "imageOnText";
+// textOnImage.innerHTML = "LeverX International";
+// document.getElementById("fe1").appendChild(textOnImage);
 
-var text1 = document.createElement('p');
-text1.className = "imageText";
-text1.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
-document.getElementById("fe1").appendChild(text1);
+// var text1 = document.createElement('p');
+// text1.className = "imageText";
+// text1.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+// document.getElementById("fe1").appendChild(text1);
 
-var button = document.createElement('button');
-button.className = "buttonOnCard";
-button.textContent = "Try";
-document.getElementById("fe1").appendChild(button);
-
-
-var flexEl2 = document.createElement('div');
-flexEl2.className = "flex-item";
-flexEl2.id = "fe2";
-document.getElementById("row").appendChild(flexEl2);
-
-var img2 = document.createElement('img');
-img2.src = "images/img.jpg";
-img2.className = "card"
-document.getElementById("fe2").appendChild(img2);
-
-var textOnImage2 = document.createElement('p');
-textOnImage2.className = "imageOnText";
-textOnImage2.innerHTML = "LeverX International";
-document.getElementById("fe2").appendChild(textOnImage2);
-
-var text2 = document.createElement('p');
-text2.className = "imageText";
-text2.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
-document.getElementById("fe2").appendChild(text2);
-
-var button2 = document.createElement('button');
-button2.className = "buttonOnCard";
-button2.textContent = "Try";
-document.getElementById("fe2").appendChild(button2);
-
-var flexEl3 = document.createElement('div');
-flexEl3.className = "flex-item";
-flexEl3.id = "fe3";
-document.getElementById("row").appendChild(flexEl3);
-
-var img3 = document.createElement('img');
-img3.src = "images/img.jpg";
-img3.className = "card"
-document.getElementById("fe3").appendChild(img3);
-
-var textOnImage3 = document.createElement('p');
-textOnImage3.className = "imageOnText";
-textOnImage3.innerHTML = "LeverX International";
-document.getElementById("fe3").appendChild(textOnImage3);
-
-var text3 = document.createElement('p');
-text3.className = "imageText";
-text3.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
-document.getElementById("fe3").appendChild(text3);
-
-var button3 = document.createElement('button');
-button3.className = "buttonOnCard";
-button3.textContent = "Try";
-document.getElementById("fe3").appendChild(button3);
-
-var flexRow2 = document.createElement('div');
-flexRow2.className = "row";
-flexRow2.id = "row2";
-document.getElementById("fc").appendChild(flexRow2);
+// var button = document.createElement('button');
+// button.className = "buttonOnCard";
+// button.textContent = "Try";
+// document.getElementById("fe1").appendChild(button);
 
 
-var flexEl4 = document.createElement('div');
-flexEl4.className = "flex-item";
-flexEl4.id = "fe4";
-document.getElementById("row2").appendChild(flexEl4);
+// var flexEl2 = document.createElement('div');
+// flexEl2.className = "flex-item";
+// flexEl2.id = "fe2";
+// document.getElementById("row").appendChild(flexEl2);
 
-var img4 = document.createElement('img');
-img4.className = "card";
-img4.src = "images/img.jpg";
-document.getElementById("fe4").appendChild(img4);
+// var img2 = document.createElement('img');
+// img2.src = "images/img.jpg";
+// img2.className = "card"
+// document.getElementById("fe2").appendChild(img2);
 
-var textOnImage4 = document.createElement('p');
-textOnImage4.className = "imageOnText";
-textOnImage4.innerHTML = "LeverX International";
-document.getElementById("fe4").appendChild(textOnImage4);
+// var textOnImage2 = document.createElement('p');
+// textOnImage2.className = "imageOnText";
+// textOnImage2.innerHTML = "LeverX International";
+// document.getElementById("fe2").appendChild(textOnImage2);
 
-var text4 = document.createElement('p');
-text4.className = "imageText";
-text4.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
-document.getElementById("fe4").appendChild(text4);
+// var text2 = document.createElement('p');
+// text2.className = "imageText";
+// text2.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+// document.getElementById("fe2").appendChild(text2);
 
-var button4 = document.createElement('button');
-button4.className = "buttonOnCard";
-button4.textContent = "Try";
-document.getElementById("fe4").appendChild(button4);
+// var button2 = document.createElement('button');
+// button2.className = "buttonOnCard";
+// button2.textContent = "Try";
+// document.getElementById("fe2").appendChild(button2);
 
-var flexEl5 = document.createElement('div');
-flexEl5.className = "flex-item";
-flexEl5.id = "fe5";
-document.getElementById("row2").appendChild(flexEl5);
+// var flexEl3 = document.createElement('div');
+// flexEl3.className = "flex-item";
+// flexEl3.id = "fe3";
+// document.getElementById("row").appendChild(flexEl3);
 
-var img5 = document.createElement('img');
-img5.className = "card";
-img5.src = "images/img.jpg";
-document.getElementById("fe5").appendChild(img5);
+// var img3 = document.createElement('img');
+// img3.src = "images/img.jpg";
+// img3.className = "card"
+// document.getElementById("fe3").appendChild(img3);
 
-var textOnImage5 = document.createElement('p');
-textOnImage5.className = "imageOnText";
-textOnImage5.innerHTML = "LeverX International";
-document.getElementById("fe5").appendChild(textOnImage5);
+// var textOnImage3 = document.createElement('p');
+// textOnImage3.className = "imageOnText";
+// textOnImage3.innerHTML = "LeverX International";
+// document.getElementById("fe3").appendChild(textOnImage3);
 
-var text5 = document.createElement('p');
-text5.className = "imageText";
-text5.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
-document.getElementById("fe5").appendChild(text5);
+// var text3 = document.createElement('p');
+// text3.className = "imageText";
+// text3.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+// document.getElementById("fe3").appendChild(text3);
 
-var button5 = document.createElement('button');
-button5.className = "buttonOnCard";
-button5.textContent = "Try";
-document.getElementById("fe5").appendChild(button5);
+// var button3 = document.createElement('button');
+// button3.className = "buttonOnCard";
+// button3.textContent = "Try";
+// document.getElementById("fe3").appendChild(button3);
 
-var flexEl6 = document.createElement('div');
-flexEl6.className = "flex-item";
-flexEl6.id = "fe6";
-document.getElementById("row2").appendChild(flexEl6);
+// var flexRow2 = document.createElement('div');
+// flexRow2.className = "row";
+// flexRow2.id = "row2";
+// document.getElementById("fc").appendChild(flexRow2);
 
-var img6 = document.createElement('img');
-img6.className = "card";
-img6.src = "images/img.jpg";
-document.getElementById("fe6").appendChild(img6);
 
-var textOnImage6 = document.createElement('p');
-textOnImage6.className = "imageOnText";
-textOnImage6.innerHTML = "LeverX International";
-document.getElementById("fe6").appendChild(textOnImage6);
+// var flexEl4 = document.createElement('div');
+// flexEl4.className = "flex-item";
+// flexEl4.id = "fe4";
+// document.getElementById("row2").appendChild(flexEl4);
 
-var text6 = document.createElement('p');
-text6.className = "imageText";
-text6.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
-document.getElementById("fe6").appendChild(text6);
+// var img4 = document.createElement('img');
+// img4.className = "card";
+// img4.src = "images/img.jpg";
+// document.getElementById("fe4").appendChild(img4);
 
-var button6 = document.createElement('button');
-button6.className = "buttonOnCard";
-button6.textContent = "Try";
-document.getElementById("fe6").appendChild(button6);
+// var textOnImage4 = document.createElement('p');
+// textOnImage4.className = "imageOnText";
+// textOnImage4.innerHTML = "LeverX International";
+// document.getElementById("fe4").appendChild(textOnImage4);
+
+// var text4 = document.createElement('p');
+// text4.className = "imageText";
+// text4.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+// document.getElementById("fe4").appendChild(text4);
+
+// var button4 = document.createElement('button');
+// button4.className = "buttonOnCard";
+// button4.textContent = "Try";
+// document.getElementById("fe4").appendChild(button4);
+
+// var flexEl5 = document.createElement('div');
+// flexEl5.className = "flex-item";
+// flexEl5.id = "fe5";
+// document.getElementById("row2").appendChild(flexEl5);
+
+// var img5 = document.createElement('img');
+// img5.className = "card";
+// img5.src = "images/img.jpg";
+// document.getElementById("fe5").appendChild(img5);
+
+// var textOnImage5 = document.createElement('p');
+// textOnImage5.className = "imageOnText";
+// textOnImage5.innerHTML = "LeverX International";
+// document.getElementById("fe5").appendChild(textOnImage5);
+
+// var text5 = document.createElement('p');
+// text5.className = "imageText";
+// text5.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+// document.getElementById("fe5").appendChild(text5);
+
+// var button5 = document.createElement('button');
+// button5.className = "buttonOnCard";
+// button5.textContent = "Try";
+// document.getElementById("fe5").appendChild(button5);
+
+// var flexEl6 = document.createElement('div');
+// flexEl6.className = "flex-item";
+// flexEl6.id = "fe6";
+// document.getElementById("row2").appendChild(flexEl6);
+
+// var img6 = document.createElement('img');
+// img6.className = "card";
+// img6.src = "images/img.jpg";
+// document.getElementById("fe6").appendChild(img6);
+
+// var textOnImage6 = document.createElement('p');
+// textOnImage6.className = "imageOnText";
+// textOnImage6.innerHTML = "LeverX International";
+// document.getElementById("fe6").appendChild(textOnImage6);
+
+// var text6 = document.createElement('p');
+// text6.className = "imageText";
+// text6.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+// document.getElementById("fe6").appendChild(text6);
+
+// var button6 = document.createElement('button');
+// button6.className = "buttonOnCard";
+// button6.textContent = "Try";
+// document.getElementById("fe6").appendChild(button6);
